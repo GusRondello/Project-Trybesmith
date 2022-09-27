48cf5ba6808bffcbfb720ec6572697cb9a92970f
GusRond <gustavorondello99@outlook.com>
2022-09-27 16:38:55 -0300

    feat: userValidate

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/users.controller';
+import validateUser from '../middlewares/users.middleware';
 
 const userRouter = Router();
 
 const usersController = new UserController();
 
-userRouter.post('/', usersController.create);
+userRouter.post('/', validateUser, usersController.create);
 
 export default userRouter;
